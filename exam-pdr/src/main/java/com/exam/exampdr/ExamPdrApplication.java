d2d0b8c61ecf14aa74bcd4265223f076a9d0a30e
moqiwhite <moqiwhite156@163.com>
2021-12-08 08:49:49 +0800

    这是提交测试

---
+++ exam-pdr/src/main/java/com/exam/exampdr/ExamPdrApplication.java
@@ -0,0 +1,17 @@
+package com.exam.exampdr;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.exam.exampdr.dao")
+public class ExamPdrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPdrApplication.class, args);
+    }
+
+}
---
+++ exam-pdr/src/main/java/com/exam/exampdr/service/ExamService.java
@@ -0,0 +1,51 @@
+package com.exam.exampdr.service;
+
+import com.alibaba.fastjson.JSON;
+import com.exam.exampdr.dao.ExamDao;
+import model.Comment;
+import model.Detail;
+import model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ExamService {
+
+    @Resource
+    private ExamDao dao;
+
+    @GetMapping("/getAllDetails")
+    List<Detail> getAllDetails(){
+        System.out.println("11111");
+        return dao.getAll();
+
+    }
+
+    @GetMapping("/getUser")
+    Map<String,Object> getUser(String userName, String password){
+
+        Map<String,Object> result = new HashMap<>();
+        result.put("data",JSON.toJSONString(dao.getUser(userName, password)));
+        result.put("fb",false);
+        return result;
+
+    }
+
+    @PostMapping("/saveComment")
+    boolean saveComment(@RequestBody Comment comment){
+        int i = dao.addComment(comment);
+        if (i>0){
+            return true;
+        }
+        return false;
+    }
+
+
+}
